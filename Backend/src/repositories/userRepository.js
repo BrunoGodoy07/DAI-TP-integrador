a32aa7d707c1a79f80279de7791177acd7d33020
Android Studio <devandroid@ort>
2025-08-11 10:16:59 -0300

    cambio chiquito

--- Backend/src/repositories/userRepository.js
+++ Backend/src/repositories/userRepository.js
@@ -5,29 +5,27 @@ import DBConfig from '../configs/DBConfig.js';
 export default class eventRepository {
     findByUsername = async (username) => {
         const client = new Client(DBConfig);
-        await client.connect();
         try {
-            const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+            const result = await
+            await client.connect(); client.query('SELECT * FROM users WHERE username = $1', [username]);
+            await client.end();
             return result.rows[0] || null;
         } catch (err) {
             console.error("error:" + err)
-        } finally {
-            await client.end();
         }
     }
 
     createUser = async ({ first_name, last_name, username, password }) => {
         const client = new Client(DBConfig);
-        await client.connect();
         try {
+            await client.connect();
             await client.query(
                 'INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)',
                 [first_name, last_name, username, password]
             );
+            await client.end();
         } catch (err) {
             console.error("error:" + err)
-        } finally {
-            await client.end();
         }
     }
 }
